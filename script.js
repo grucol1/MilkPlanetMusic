f2a769cbedb142f0e35c15d5cda1949c6ee976b3
grucol1 <131767426+grucol1@users.noreply.github.com>
2023-04-25 20:19:53 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-function updateTime() {
-  const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
-  const clock = document.getElementById('clock');
-  clock.textContent = `${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-}
+const endTime = new Date('December 31, 2023 23:59:59').getTime();
+const catMeow = document.getElementById('cat-meow');
+const scaryNoise = document.getElementById('scary-noise');
 
-updateTime();
-setInterval(updateTime, 1000);
+function updateCountdown() {
+  const now = new Date().getTime();
+  const timeRemaining = endTime - now;
+
+  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+  document.getElementById('days').textContent = `${days}d `;
+  document.getElementById('hours').textContent = `${hours < 10 ? '0' : ''}${hours}:`;
+  document.getElementById('minutes').textContent = `${minutes < 10 ? '0' : ''}${minutes}:`;
